fd2c5899fff7af5a5a907564e9882e9b9ca1ae14
Khue Pham <veepahm@gmail.com>
2023-07-24 11:44:21 +0700

    remove metadata

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -9,17 +9,17 @@ import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export async function generateMetadata({ params }) {
-  const post = await getPost(params.id);
-  if (!post.title) {
-    return {
-      title: "User Not Found",
-    };
-  }
-  return {
-    title: post.title,
-  };
-}
+// export async function generateMetadata({ params }) {
+//   const post = await getPost(params.id);
+//   if (!post.title) {
+//     return {
+//       title: "User Not Found",
+//     };
+//   }
+//   return {
+//     title: post.title,
+//   };
+// }
 
 const Post = ({ params }) => {
   const { data, error } = useSWR(`/api/posts/${params.id}`, fetcher);
