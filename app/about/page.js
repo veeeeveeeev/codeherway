51f93be8ad66995f0eb7a937c915e206bfbd7870
Khue Pham <veepahm@gmail.com>
2023-11-01 16:43:46 +0700

    fix resume

--- app/about/page.js
+++ app/about/page.js
@@ -6,7 +6,7 @@ const About = () => {
       <AboutTop />
       <AboutBody
         aboutImage="/assets/square.jpg"
-        link={{ href: "/assets/resume.pdf" }}
+        link={{ href: "/resume.pdf" }}
         textLink="View resume"
         introText="I am a self-taught programmer, and was able to create this website from scratch! My two biggest hobbies are coding and designing. I love exploring new things, hanging out with friends, binging shows, doing puzzles, visiting museums, going out for cafes, planning, journaling, going on road trips, and so much more!"
       />
